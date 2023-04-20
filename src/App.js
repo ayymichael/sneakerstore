5518bfc261119b234d0e566e738dba60de5250d5
Mikhail Panferov <m_panferov@inbox.ru>
2023-04-21 00:48:45 +0300

    верстка поиска

--- src/App.js
+++ src/App.js
@@ -2,37 +2,44 @@ import Card from './Card.js'
 
 
 function App() {
-  return (
-    <div className="wrapper">
-      <header>
-        <div className="leftSide"> 
-          <img src="logo3.jpg" alt="logo"></img>
-          <div className="headerInfo">
-            <h3>hold in line</h3>
-            <p>Магазин оригинальных кроссовок</p>
-          </div>
-        </div>
-        <div className="rightSide">
-          <p>Корзина</p>
-          <p>Войти</p>
-        </div>
-      </header>
+    return (
+        <div className="wrapper">
+            <header>
+                <div className="leftSide">
+                    <img src="logo3.jpg" alt="logo"></img>
+                    <div className="headerInfo">
+                        <h3>hold in line</h3>
+                        <p>Магазин оригинальных кроссовок</p>
+                    </div>
+                </div>
+                <div className="rightSide">
+                    <p>Корзина</p>
+                    <p>Войти</p>
+                </div>
+            </header>
 
-    <div className="content">
-      <div>
-        <h2>
-          All shoes
-        </h2>
-      </div>
-      <div className="items">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-      </div>
-    </div>
-    </div>
-  );
+            <div className="content">
+                <div className='d-flex a-center justify-between'>
+                    <h2>
+                        Все кроссовки
+                    </h2>
+
+                    <div className='search-block d-flex'>
+                        <img src='/img/search.svg' alt='search'></img>
+                        <input placeholder='Поиск...'></input>
+                    </div>
+                </div>
+            
+                <div className="items">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
